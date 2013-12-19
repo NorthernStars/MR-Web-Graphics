5c4f75bc5e736a45acf62f56b91977367624a6b9
WerthersEchte <arabali@gmx.de>
2013-12-19 21:41:05 +0100

    fixed addgames templates

--- public/scripts/gameadmin.js
+++ public/scripts/gameadmin.js
@@ -1,7 +1,8 @@
 var onsubmitNewGameForm = function(){
-	var gamename = document.getElementById('').value;
-	var host = document.getElementById('').value;
-	var port = document.getElementById('').value;
-	
+
+	var gamename = document.getElementById('newConnectionGameName').value;
+	var host = document.getElementById('newConnectionIP').value;
+	var port = document.getElementById('newConnectionPort').value;
+
 	post_to_url( '/admin/game/add', {name: gamename, host: host, port: port} );
 };
