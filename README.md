5224c7ea5f7905b22285ed0603987f89368326f3
WerthersEchte <arabali@gmx.de>
2014-02-02 19:01:00 +0100

    settings comments and readme fixes

--- README.md
+++ README.md
@@ -1,26 +1,28 @@
 MR-Web-Graphics
 ===============
-This projects contains a browser interface for the Mixed-Reality Graphics module.
-It bases on a Node.js server, that connects to the Mixed-Reality Gameserver and offers a webpage to display games.
-
+This projects contains a browser interface for the Mixed-Reality Graphics module.  
+It bases on a Node.js server, that connects to the Mixed-Reality Gameserver and offers a webpage to display games.  
+  
 Installation
 ------------
-First install Node.js (http://nodejs.org/).
-Than download complete project and execute on projects root directory:
-
-sudo apt-get install npm
+First install Node.js (http://nodejs.org/).  
+Than download complete project and execute on projects root directory:  
+  
+sudo apt-get install npm  
 npm install
-
-Create database from projects root directory.
+  
+Create database from projects root directory.  
 node setup_db.js 
 
+To configuer the server edit  
+settings.js 
 
 Usage
 -----
-To start the server execute on projects root directory:
-
-node core/servercore
-
-After that you can access the server through your browser on
-
-localhost:3000
+To start the server execute on projects root directory:  
+  
+node core/servercore  
+  
+After that you can access the server through your browser on  
+  
+localhost:3000  
--- settings.js
+++ settings.js
@@ -15,28 +15,39 @@ module.exports = (function(){
 
     var that = {
             core: {
+                // values to secure the sessions on the clients
                 session: {
+                    // the name of the sessioncookie
                     key:  'mrwg-session',
+                    // the secret to secure the sessiondata
                     secret: 'dasi5teinwursttanz',
+                    // the lifetime of the sessioncookie, eg the lifetime of the session
                     cookie: {
-                        maxAge: 1000 * 60 * 20 // 20 minuites lifetime
+                        maxAge: 1000 * 60 * 20 // 20 minutes lifetime
                     }
                 },
     			server: {
+    			    // the serverport, should be higer than 1024 and http should be forwarded to it
     				port: 3000,
+    				// the websocketconfiguration
     				websocket: {
-    					ip: '192.168.178.21',
+    				    // the url(ip) of the webserver to allow the websocket to connect to it
+    					ip: 'myserver',
+    					// the websocketport, can be the same as http port
     					port: '3000'
     				}
     			},
                 cache:{
+                    // the lifetme of the clientside cacheobjects
                     lifetime: 1000 * 60 * 20 // 20 minuites lifetime
                 }
             },
             logging: {
+                // the configurationfile for the logger
                 config: 'log_configuration.json'
             },
             user:{
+                // the name of the database
                 database: './admin.db'
             }
     };
